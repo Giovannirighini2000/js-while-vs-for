14618ed176cbdbd635af88de9840b562b4df18c3
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-02-07 16:11:28 +0100

    feat added edit code

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const numeriDispariTot = []
 // UTILIZZIAMO IL CICLO WHILE PER RIPETERE FINCHE IL COUNTER (10DISPARI ) NON SI EFFETTUA
 while(start<counter){
     chiedoNumeri = parseInt(prompt('inserisci un numero dispari'))
-    console.log(chiedoNumeri,'PARI E DISPARI')
+    console.log(chiedoNumeri,'PARI O DISPARI')
     resto=chiedoNumeri % 2 
     // SE E DISPARI MELE SALVO NEL CONTEGGIO DEL COUNTER
     if(resto === 1){
@@ -19,10 +19,11 @@ while(start<counter){
         console.log(numeriDispariTot,'DISPARI')
     }   
 }
-
 // CONTROLLIAMO I NUMERI DISPARI ATTRAVERSO L'INDICES
 for(let i = 0;i < numeriDispariTot.length;i++){
     
     console.log(numeriDispariTot[i])
+    
 }
 
+    
