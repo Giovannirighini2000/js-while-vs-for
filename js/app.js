26cfeeac521f57490ed836f611404e196704bac2
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-02-07 16:02:31 +0100

    FEAT EDIT code js delite and added comment

--- js/app.js
+++ js/app.js
@@ -2,27 +2,25 @@
 let resto 
 let start = 0
 let counter = 10
+
 // CHIEDO ALL'UTENTE UN NUMERO
 let chiedoNumeri
 const numeriDispariTot = []
+// UTILIZZIAMO IL CICLO WHILE PER RIPETERE FINCHE IL COUNTER (10DISPARI ) NON SI EFFETTUA
 while(start<counter){
     chiedoNumeri = parseInt(prompt('inserisci un numero dispari'))
-    console.log(chiedoNumeri)
-    resto=chiedoNumeri % 2
-    // SE E PARI NON LO SALVO E SI RIPETE IL LOOP  
-    if(resto === 0){
-        console.log('PARI')
-    }
-    // SE E DISPARI MELE SALVO NEL COUNTER
-    else if(resto === 1){
-        console.log('DISPARI')
+    console.log(chiedoNumeri,'PARI E DISPARI')
+    resto=chiedoNumeri % 2 
+    // SE E DISPARI MELE SALVO NEL CONTEGGIO DEL COUNTER
+    if(resto === 1){
+        
         start++
         numeriDispariTot.push(chiedoNumeri)
-        console.log(numeriDispariTot)
-    }
-
-    
+        console.log(numeriDispariTot,'DISPARI')
+    }   
 }
+
+// CONTROLLIAMO I NUMERI DISPARI ATTRAVERSO L'INDICES
 for(let i = 0;i < numeriDispariTot.length;i++){
     
     console.log(numeriDispariTot[i])
